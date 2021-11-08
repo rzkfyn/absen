93733ebf4c39fd6e1274cd83349db3ded4561a1c
Rizki Sofyan <rzkfyn@gmail.com>
2021-11-08 18:19:37 +0700

    refactoring

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
 const wa = require('@open-wa/wa-automate');
 const noWakel = process.env.NO_WAKEL;
-const absen = [
-    "SMK BISA", 
-    "SMK HEBAT", 
-    "SMK BERTANGGUNGJAWAB", "SMK BERTANGGUNG JAWAB", "SMK TANGGUNG JAWAB", "SMK TANGGUNGJAWAB",
-    "SMK BERAKHLAQ MULIA", "SMK BERAKHLAK MULIA",
-    "SMK AMANAH",
-    "SMK BERINTEGRITAS",
-    "SMK IDAMAN",
-    "SMK BISA",
-    "SMK AMANAH"
-];
 
-wa.create({
-    sessionId: "idk",
-    authTimeout: 60,
-    blockCrashLogs: true,
-    disableSpins: true,
-    headless: true,
-    hostNotificationLang: 'PT_BR',
-    logConsole: false,
-    popup: true,
-    qrTimeout: 0,
-}).then(bot => start(bot));
+wa.create({sessionId: "idk",authTimeout: 60,blockCrashLogs: true,disableSpins: true,headless: true,hostNotificationLang: 'PT_BR',logConsole: false,popup: true,qrTimeout: 0,})
+  .then(bot => start(bot));
 
 const start = bot => {
     bot.onMessage(async message => {
       console.log(message);
-        if(message.author !== noWakel) return;
+        if(message.author !== noWakel || message.body.toUpperCase().split('*').length < 2) return;
         for(let a of message.body.toUpperCase().split('*')){
-            for(let b of absen){
-                if(a === b) return await bot.sendText(message.from, `*${b}*`);
-            }
+              if(a.startsWith('SMK')) return await bot.sendText(message.from, `*${a}*`);
         }
     });
 }
